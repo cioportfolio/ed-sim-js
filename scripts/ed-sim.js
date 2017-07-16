696e375f9ead356171a0068d5713294fe6e84a97
Richard Barton <richard.barton@example.com>
2017-07-16 12:24:04 +0100

    Put more realistic numbers

--- scripts/ed-sim.js
+++ scripts/ed-sim.js
@@ -1,49 +1,50 @@
+const doctorsOnDuty = 5.0;
+const treatmentTime = 20.0;
+const MeanArrival = 15.0;
+const triageTime = 5.0;
+const Seed = 1234.0;
+const Simtime = 1440.0;
+
+
 function edSim(
-        doctorsOnDuty, 
-        treatmentTime, 
-        MeanArrival, 
-        triageTime, 
-        Seed, 
-        Simtime) 
+) 
 {
     var sim = new Sim(); 
     var stats = new Sim.Population();
     var triageNurse = new Sim.Facility('triageNurse');
-    var doctors = new Sim.Buffer('doctors', doctorsOnDuty);
+    var doctors = new Sim.Facility('doctors', Sim.Facility.FCFS, doctorsOnDuty);
     var random = new Random(Seed);
     
     var Patient = {
         start: function () {
-            this.order();
+            this.triage();
             
             var nextPatientAt = random.exponential (1.0 / MeanArrival); 
             this.setTimer(nextPatientAt).done(this.start);
         },
         
-        order: function () {
+        triage: function () {
             sim.log("Patient ENTER at " + this.time());
             stats.enter(this.time());
             
-            this.getBuffer(doctors, 1).done(function () {
-                sim.log("Patient at triageNurse " + this.time() + " (entered at " + this.callbackData + ")");
-                var serviceTime = random.exponential(1.0 / triageTime);
-                this.useFacility(triageNurse, serviceTime).done(function () {
-                    sim.log("Patient LEAVE at " + this.time() + " (entered at " + this.callbackData + ")");
-                    stats.leave(this.callbackData, this.time());
-                }).setData(this.callbackData);
+            this.useFacility(triageNurse, triageTime).done(function () {
+                sim.log("Patient triaged at " + this.time() + " (started at " + this.callbackData + ")");
+                this.treatment();
+            }).setData(this.time());
+        },
+
+        treatment: function () {
+            sim.log("Patient treated at " + this.time());
+            
+            this.useFacility(doctors, treatmentTime).done(function () {
+                sim.log("Patient treated at " + this.time() + " (started at " + this.callbackData + ")");
+                stats.leave(this.callbackData, this.time());
+                
             }).setData(this.time());
         }
     };
-    
-    var Doctor = {
-        start: function () {
-            this.putBuffer(doctors, doctorsOnDuty - doctors.current());
-            this.setTimer(treatmentTime).done(this.start);
-        }
-    };
-    
+        
     sim.addEntity(Patient);
-    sim.addEntity(Doctor);
     
 //  Uncomment these line to display logging information
 //    sim.setLogger(function (msg) {
